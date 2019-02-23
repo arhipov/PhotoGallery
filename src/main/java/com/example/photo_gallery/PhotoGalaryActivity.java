66d8abf703335b5c998f49a7101a54dfeb1ba141
gegemon <0601422@gmail.com>
2019-02-23 13:44:55 +0300

    Начало

---
+++ src/main/java/com/example/photo_gallery/PhotoGalaryActivity.java
@@ -0,0 +1,14 @@
+package com.example.photo_gallery;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGalaryActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return PhotoGalaryFragment.newInstance();
+    }
+
+}

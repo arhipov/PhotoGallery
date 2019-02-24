aa117084982910cff25d20a4be1abcec3716a8e3
gegemon <0601422@gmail.com>
2019-02-24 18:50:00 +0300

    5 часть

--- src/main/java/com/example/photo_gallery/PhotoGalaryActivity.java
+++ src/main/java/com/example/photo_gallery/PhotoGalaryActivity.java
@@ -1,10 +1,15 @@
 package com.example.photo_gallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PhotoGalaryActivity extends SingleFragmentActivity {
+    public static Intent newIntent(Context context){
+        return new Intent(context, PhotoGalaryActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {
